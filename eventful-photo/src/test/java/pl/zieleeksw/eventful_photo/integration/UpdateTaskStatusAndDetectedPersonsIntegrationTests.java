98c7c93f03c587e1e046ccd712eff27e36baeb14
Szymon Zieliński <szielinski1508@wp.pl>
2025-01-26 14:14:42 +0100

    Create integration tests for put mappings

---
+++ eventful-photo/src/test/java/pl/zieleeksw/eventful_photo/integration/UpdateTaskStatusAndDetectedPersonsIntegrationTests.java
@@ -0,0 +1,58 @@
+package pl.zieleeksw.eventful_photo.integration;
+
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MvcResult;
+import pl.zieleeksw.eventful_photo.task.dto.StatusDto;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class UpdateTaskStatusAndDetectedPersonsIntegrationTests extends BaseIntegration {
+
+    @Test
+    void shouldUpdateTaskStatus() throws Exception {
+        byte[] imageBytes = "image-data".getBytes();
+        MockMultipartFile file = new MockMultipartFile("file", "image.jpg", "image/jpeg", imageBytes);
+
+        MvcResult result = mockMvc.perform(multipart("/api/v1/tasks")
+                        .file(file)
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andReturn();
+
+        UUID id = UUID.fromString(JsonPath.read(result.getResponse().getContentAsString(), "$.id"));
+
+        mockMvc.perform(get("/api/v1/tasks/{taskId}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(StatusDto.PENDING.toString()))
+                .andExpect(jsonPath("$.detectedPersons").value(0));
+
+        String json = """
+                {
+                    "status": "COMPLETED",
+                    "detectedPersons": 7
+                }
+                """;
+
+        mockMvc.perform(put("/api/v1/tasks/{id}/status/detected-persons", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/v1/tasks/{taskId}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(StatusDto.COMPLETED.toString()))
+                .andExpect(jsonPath("$.detectedPersons").value(7));
+    }
+}
