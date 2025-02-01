ca8b215c8583d514ddd3368ee153774290d92152
Szymon Zieliński <szielinski1508@wp.pl>
2025-02-01 18:42:19 +0100

    Adding automatic start stop for microservices in ps1

--- start_microservices.ps1
+++ start_microservices.ps1
@@ -6,6 +6,13 @@ mvn clean package -DskipTests
 docker build -t eventful-photo .
 Set-Location ..
 
+Write-Host "Building photo-analysis..."
+Set-Location photo-analysis
+mvn clean package -DskipTests
+docker build -t photo-analysis .
+Set-Location ..
+
+
 Write-Host "Running docker-compose..."
 docker-compose up -d
 
@@ -15,5 +22,9 @@ Start-Sleep -Seconds 10
 Write-Host "Running microservices..."
 
 docker run -d -p 8080:8080 --name eventful-photo --network person_detector_microservices_network eventful-photo
+docker run -d -p 8081:8081 --name photo-analysis --network person_detector_microservices_network photo-analysis
+#docker run -d -p 8082:8082 --name photo-analysis-second --network person_detector_microservices_network photo-analysis
+Write-Host "Waiting 10 seconds for microservices..."
+Start-Sleep -Seconds 10
 
 Write-Host "Everything done!"
--- stop_microservices.ps1
+++ stop_microservices.ps1
@@ -1,12 +1,19 @@
 Write-Host "Stopping microservices..."
 docker stop eventful-photo 2>$null
+docker stop photo-analysis 2>$null
+#docker stop photo-analysis-second 2>$null
 
 docker rm eventful-photo 2>$null
+docker rm photo-analysis 2>$null
+#docker rm photo-analysis-second 2>$null
+
 
 Write-Host "Stopping docker-compose..."
 docker-compose down
 
 Write-Host "Deleting images..."
 docker rmi eventful-photo 2>$null
+docker rmi photo-analysis 2>$null
+#docker rmi photo-analysis-second 2>$null
 
 Write-Host "Cleaning done!"
